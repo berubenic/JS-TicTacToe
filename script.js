d0663c5cc2656f29abf6721b5687afa99e9e7cf3
berubenic <berubenic@gmail.com>
2021-12-22 15:55:44 -0500

    game logic

--- script.js
+++ script.js
@@ -2,7 +2,108 @@
 
 const gameBoard = (function () {
   let board = document.querySelectorAll(".row");
-  return { board };
+
+  return { board, addPlayToBoard };
+
+  function addPlayToBoard(cell) {
+    let player = gameController.currentPlayer();
+    let playerType = player.type;
+    if (_isCellOccupied(cell)) {
+      return gameController.cellAlreadyOccupied();
+    }
+    cell.innerHTML = playerType;
+    if (_isWinningSequence(playerType)) {
+      return gameController.gameWon(player);
+    } else if (_isTie()) {
+      return gameController.gameTie();
+    }
+    gameController.changeTurn();
+  }
+
+  function _isTie() {
+    for (let rowIndex = 0; rowIndex < board.length; rowIndex++) {
+      let cells = board[rowIndex].children;
+      for (let cellIndex = 0; cellIndex < cells.length; cellIndex++) {
+        if (cells[cellIndex].innerHTML === "") {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  function _isCellOccupied(cell) {
+    if (cell.innerHTML === "") {
+      return false;
+    }
+    return true;
+  }
+
+  function _isWinningSequence(winningType) {
+    if (_isRowWin(winningType)) {
+      return true;
+    } else if (_isColWin(winningType)) {
+      return true;
+    } else if (_isDiagWin(winningType)) {
+      return true;
+    }
+    return false;
+  }
+
+  function _isRowWin(winningType) {
+    for (let rowIndex = 0; rowIndex < board.length; rowIndex++) {
+      let row = board[rowIndex].children;
+      if (innerLoop(row)) {
+        return true;
+      }
+    }
+    function innerLoop(row) {
+      for (let cellIndex = 0; cellIndex < row.length; cellIndex++) {
+        let cell = board[cellIndex];
+        if (cell.innerHTML !== winningType) {
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+
+  function _isColWin(winningType) {
+    for (let colIndex = 0; colIndex < board.length; colIndex++) {
+      let rows = [board[0].children, board[1].children, board[2].children];
+      if (innerLoop(rows, colIndex, winningType)) {
+        return true;
+      }
+    }
+    function innerLoop(rows, colIndex, winningType) {
+      for (let rowIndex = 0; rowIndex < board.length; rowIndex++) {
+        if (rows[rowIndex][colIndex].innerHTML !== winningType) {
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+
+  function _isDiagWin(winningType) {
+    let rows = [board[0].children, board[1].children, board[2].children];
+    if (
+      rows[0][0].innerHTML === winningType &&
+      rows[1][1].innerHTML === winningType &&
+      rows[2][2].innerHTML === winningType
+    ) {
+      return true;
+    } else if (
+      rows[0][2].innerHTML === winningType &&
+      rows[1][1].innerHTML === winningType &&
+      rows[2][0].innerHTML === winningType
+    ) {
+      return true;
+    }
+    return false;
+  }
 })();
 
 // Player factory
@@ -15,6 +116,41 @@ const playerFactory = (name, type) => {
 
 let playerOne = playerFactory("Player One", "X");
 let playerTwo = playerFactory("Player Two", "O");
+
+// game moduler
+
+const gameController = (function () {
+  let playerTurn = 1;
+  let announcementHeader = document.getElementById("game_announcement");
+
+  return { currentPlayer, gameTie, gameWon, changeTurn };
+
+  function currentPlayer() {
+    if (playerTurn === 1) {
+      return playerOne;
+    } else if (playerTurn === 2) {
+      return playerTwo;
+    }
+  }
+
+  function changeTurn() {
+    if (playerTurn === 1) {
+      playerTurn = 2;
+    } else {
+      playerTurn = 1;
+    }
+  }
+
+  function gameTie() {
+    announcementHeader.innerHTML = "Oh no! It's a tie!";
+  }
+
+  function gameWon() {
+    player = currentPlayer();
+    console.log(player);
+    announcementHeader.innerHTML = `Boom! ${player.name} wins!`;
+  }
+})();
 
 // display module
 
@@ -90,7 +226,7 @@ const submitChangeNameBtn = document
     for (let cellIndex = 0; cellIndex < cells.length; cellIndex++) {
       let cell = cells[cellIndex];
       cells[cellIndex].addEventListener("click", function () {
-        addPlayToBoard(cell, rowIndex, cellIndex);
+        gameBoard.addPlayToBoard(cell);
       });
     }
   }
